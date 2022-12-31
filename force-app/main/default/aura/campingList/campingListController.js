50683a3d624b09f81d0d49221867598fada5c9e2
takaHAL <takaonoyuki0610@gmail.com>
2022-12-31 22:07:32 +0900

    コントローラー利用

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,26 +1,24 @@
 ({
   /* eslint-disable */
-  clickCreateItem: function (component, event, helper) {
-    var validCamping = component
-      .find("CampingItem")
-      .reduce(function (validsoFar, inputCmp) {
-        inputCmp.showHelpMessageIfInvalid();
-        return validsoFar && inputCmp.get("v.validity").valid;
-      }, true);
-    if (validCamping) {
-      var newCamping = component.get("v.newItem");
-      var theCamping = component.get("v.items");
-      var addCamping = JSON.parse(JSON.stringify(newCamping));
+  doInit: function (component, event, helper) {
+    var action = component.get("c.getItems");
+    action.setCallback(this, function (response) {
+      var state = response.getState();
 
-      theCamping.push(addCamping);
-      component.set("v.items", theCamping);
-      component.set("v.newItem", {
-        sobjectType: "Camping_Item__c",
-        Name: "",
-        Quantity__c: 0,
-        Price__c: 0,
-        Packed__c: false
-      });
+      if (component.isValid() && state === "SUCCESS") {
+        component.set("v.items", response.getReturnValue());
+      }
+    });
+
+    $A.enqueueAction(action);
+  },
+  clickCreateItem: function (component, event, helper) {
+    helper.validateFields(component, component.find("name"));
+    helper.validateFields(component, component.find("Price"));
+    helper.validateFields(component, component.find("Quantity"));
+    if (component.get("v.er") === false) {
+      var Item = component.get("v.newItem");
+      helper.createItem(component, Item);
     }
   }
 });
