97d81392039aadbd10b5798373d8b3da0fc17384
takaHAL <takaonoyuki0610@gmail.com>
2022-12-31 21:53:17 +0900

    データの入力

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,4 +1,26 @@
 ({
   /* eslint-disable */
-  myAction: function (component, event, helper) {}
+  clickCreateItem: function (component, event, helper) {
+    var validCamping = component
+      .find("CampingItem")
+      .reduce(function (validsoFar, inputCmp) {
+        inputCmp.showHelpMessageIfInvalid();
+        return validsoFar && inputCmp.get("v.validity").valid;
+      }, true);
+    if (validCamping) {
+      var newCamping = component.get("v.newItem");
+      var theCamping = component.get("v.items");
+      var addCamping = JSON.parse(JSON.stringify(newCamping));
+
+      theCamping.push(addCamping);
+      component.set("v.items", theCamping);
+      component.set("v.newItem", {
+        sobjectType: "Camping_Item__c",
+        Name: "",
+        Quantity__c: 0,
+        Price__c: 0,
+        Packed__c: false
+      });
+    }
+  }
 });
